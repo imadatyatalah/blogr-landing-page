2d13e752063eddab14b5903c3d14308bc54f2ab5
imad <imadatyatalah@gmail.com>
2021-03-15 19:20:57 +0100

    Import Overpass and Ubuntu fonts and index.css file

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,3 +1,14 @@
+// Overpass font
+import "@fontsource/overpass/latin-300.css";
+import "@fontsource/overpass/latin-600.css";
+
+// Ubuntu font
+import "@fontsource/ubuntu/latin-300.css";
+import "@fontsource/ubuntu/latin-500.css";
+import "@fontsource/ubuntu/latin-700.css";
+
+import "../styles/index.css";
+
 const MyApp = ({ Component, pageProps }) => (
   <>
     <Component {...pageProps} />
