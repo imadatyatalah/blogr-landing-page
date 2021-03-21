06d418e2dd06868ecdfaca6f7ddfa7bf49d5f67e
imad <imadatyatalah@gmail.com>
2021-03-21 20:23:51 +0100

    Added links

---
+++ config.js
@@ -0,0 +1,20 @@
+export const links = [
+  {
+    title: "Product",
+    children: [
+      "Overview",
+      "Pricing",
+      "Marketplace",
+      "Features",
+      "Integrations",
+    ],
+  },
+  {
+    title: "Company",
+    children: ["About", "Team", "Blog", "Careers"],
+  },
+  {
+    title: "Connect",
+    children: ["Contact", "Newsletter", "LinkedIn"],
+  },
+];
